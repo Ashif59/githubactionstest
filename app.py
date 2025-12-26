270c07c3a3c9088886e216db8c53d4f837151194
ashif59 <ashifnavheed4009@gmail.com>
2025-12-26 13:19:50 +0000

    test the api endpoint i made

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/health')
+def health_check():
+    """
+    This endpoint returns the health of the application.
+    """
+    return jsonify({"status": "healthy"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
